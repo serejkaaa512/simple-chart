2f4d247b75833442c15d1df63468d570ca083b2b
serejkaaa512 <5125402@mail.ru>
2016-03-31 09:59:32 +0300

    added macros and moved graph to file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+#[macro_use]
 extern crate simple_graph;
-
 use simple_graph::graph;
 
+
 fn main() {
-    let p = vec!(
-        graph::Point{ x:1f64, y:1f64}, 
-        graph::Point{ x:2f64, y:2f64}, 
-        graph::Point{ x:3f64, y:3f64});
-    let res = graph::create (p.iter(), "./example/graph.bmp", 740, 480);  
+    let p = vec![graph::Point { x: 1f64, y: 1f64 },
+                 graph::Point { x: 2f64, y: 2f64 },
+                 graph::Point { x: 3f64, y: 3f64 }];
+    let res = graph::create(p.iter(), "./example/graph.bmp", 740, 480);
     println!("{:?}", res.unwrap());
 }
